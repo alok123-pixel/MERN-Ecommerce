e961491b5663ee5a6540f26558893eb2ee704af2
Alok123-pixel <mshlok75@gmail.com>
2026-01-17 16:45:14 +0530

    changes

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
     try {
+        mongoose.set('bufferCommands', false);
+        mongoose.set('bufferMaxEntries', 0);
         mongoose.connection.on('connected',()=>{
             console.log('DB connection ho gaya');
         })
@@ -11,12 +13,7 @@ const connectDB = async()=>{
         mongoose.connection.on('disconnected', () => {
             console.log('DB disconnected');
         });
-        await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            bufferCommands: false, // Disable mongoose buffering
-            bufferMaxEntries: 0 // Disable mongoose buffering
-        })
+        await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
     } catch (error) {
         console.log('DB connection failed:', error)
         process.exit(1); // Exit if DB connection fails
